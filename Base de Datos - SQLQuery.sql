ee3cc9a039f4b780997be0ebb1217f620d252d50
sebastillar <sebastian.villar@outlook.com>
2017-09-10 18:55:08 -0600

    Scripts SQL para Insert de Servicio, TipoEvento, TipoEventoYServicio
    Inserts con datos de prueba para las tablas Servicio, TipoEvento y
    TipoEventoYServicio

--- Base de Datos - SQLQuery.sql
+++ Base de Datos - SQLQuery.sql
@@ -9,12 +9,52 @@ Select * From ProveedorVip
 -- Insertar Admin - Clave: Administrador
 insert into Usuario Values('admin','750F9277BEF0489D9D309F267435F5874F4D173EA0E178F513D43EB86B7CA296DE51669E1BD167EC50F81D7AEF7DE10FF3F682028BE02D7815839DB33D6EB3D0', 1);
 
+-- Insertar Servicios
+INSERT INTO Servicio
+VALUES ('Fotografía', 'Fotografía integral para fiestas y eventos', '')
+
+INSERT INTO Servicio
+VALUES ('Catering', 'Catering para eventos empresariales', '')
+
+INSERT INTO Servicio
+VALUES ('Wedding planner', 'Para que tu boda sea tal cual la imaginas', '')
+
+--Insertar Tipos de eventos
+INSERT INTO TipoEvento
+VALUES ('Boda', 'Servicios para ceremonias religiosas, registro civil y fiestas')
+
+INSERT INTO TipoEvento
+VALUES ('Eventos empresariales', 'Servicios para que su empresa tenga los mejores eventos')
+
+INSERT INTO TipoEvento
+VALUES ('Fiesta de 15', 'Todo lo que necesitas para celebrar los quince años.')
+
+--Insertar Tipos de eventos y servicios
+-- Fotografia y Boda
+INSERT INTO TipoEventoYServicio
+VALUES (1,1)
+
+--Fotografia y eventos empresariales
+INSERT INTO TipoEventoYServicio
+VALUES (1,2)
+
+--Fotografia y Fiesta de 15
+INSERT INTO TipoEventoYServicio
+VALUES (1,3)
+
+--Catering y eventos empresariales
+INSERT INTO TipoEventoYServicio
+VALUES (2,2)
+
+--Wedding planner y boda
+INSERT INTO TipoEventoYServicio
+VALUES (3,1)
+
 -- Seleccionar servicios con tipos de evento asociados a cada servicio
 SELECT s.nombre, s.descripcion, s.imagen, t.nombre 
 FROM Servicio AS s 
 INNER JOIN TipoEventoYServicio AS e ON s.idServicio = e.idServicio
 INNER JOIN TipoEvento AS t ON e.idTipoEvento = t.idTipoEvento
-
 
 -- Crear la DataBase
 CREATE DATABASE ObligatorioP3
