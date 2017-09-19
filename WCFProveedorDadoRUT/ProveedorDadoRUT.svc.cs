0f54d5ecae43cf213c9bc67ff9e2c35a0c2de835
Polachek <guillermollana@gmail.com>
2017-09-19 14:14:44 -0300

    WCF Provedor por Rut

---
+++ WCFProveedorDadoRUT/ProveedorDadoRUT.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Dominio;
+
+namespace WCFProveedorDadoRUT
+{
+    public class ProveedorDadoRUT : IProveedorDadoRUT
+    {
+        public DtoProveedor buscarProveedorRut(string rut)
+        {
+            Proveedor miprov = Proveedor.FindByRUT(rut);
+
+            if(miprov == null)
+            {
+                return null;
+            }else
+            {
+                DtoProveedor miDtoProv = new DtoProveedor();
+
+                miDtoProv.RUT = miprov.RUT;
+                miDtoProv.NombreFantasia = miprov.NombreFantasia;
+                miDtoProv.Email = miprov.Email;
+                miDtoProv.Telefono = miprov.Telefono;
+                miDtoProv.FechaRegistro = miprov.FechaRegistro;
+                miDtoProv.esInactivo = miprov.esInactivo;
+                miDtoProv.Tipo = miprov.Tipo;
+                miDtoProv.ListaServicios = Servicio.FindServiciosProveedor(miDtoProv.RUT);
+
+                return miDtoProv;
+            }
+        }
+    }
+}
