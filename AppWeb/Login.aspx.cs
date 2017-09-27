0d83977cd9fbe9ea64d90b04b25ec6113e4d909b
sebastillar <30783682+sebastillar@users.noreply.github.com>
2017-09-27 02:48:49 +0200

    Tarea 4 + Cliente Prueba Consola WCF

--- AppWeb/Login.aspx.cs
+++ AppWeb/Login.aspx.cs
@@ -13,6 +13,7 @@ namespace AppWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
             if (!IsPostBack)
             {
                 Session["usu"] = null;
